1dda1c90ea3d02dfbe72635e03e657fea7846486
dayoff <124113144+sowh5462@users.noreply.github.com>
2023-06-01 13:50:53 +0900

    staff pay

--- project/src/main/java/com/example/controller/PayController.java
+++ project/src/main/java/com/example/controller/PayController.java
@@ -3,6 +3,7 @@ package com.example.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import java.util.*;
 
 import com.example.dao.PayDAO;
 
@@ -19,6 +20,18 @@ public class PayController {
 	@RequestMapping("/total")
 	public int staffTotal(int use_work_num) {
 		return dao.staffTotal(use_work_num);
+	}
+	@RequestMapping("/temp")
+	public int tempPaySum(int use_work_num) {
+		return dao.tempPaySum(use_work_num);
+	}
+	@RequestMapping("/type")
+	public List<HashMap<String, Object>> staffType(int use_work_num) {
+		return dao.staffType(use_work_num);
+	}
+	@RequestMapping("/month")
+	public List<HashMap<String, Object>> payMonth(int use_work_num) {
+		return dao.payMonth(use_work_num);
 	}
 
 }
