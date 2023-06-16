2abe14988146d7316a132a89fc39e8471051f1bf
wkdtkgkek16 <124113037+wkdtkgkek16@users.noreply.github.com>
2023-06-16 10:21:43 +0900

    StaffSchedule

--- project/src/main/java/com/example/dao/ScheduleDAO.java
+++ project/src/main/java/com/example/dao/ScheduleDAO.java
@@ -12,6 +12,8 @@ import com.example.domain.UserVO;
 public interface ScheduleDAO {
 	public List<ScheduleVO> scheduleList(int use_id);
 	public List<HashMap<String, Object>> scheduleList1(int use_work_num);
+	public List<HashMap<String, Object>> scheduleList2(int use_id);
+	public List<ScheduleVO> scheduleListStaff(int use_id);
 	public List<ScheduleVO> scheCheck(String sche_start, int use_work_num);
 	public List<UserVO> userList(int use_work_num);
 	public void scheInsert(ScheduleVO vo);
